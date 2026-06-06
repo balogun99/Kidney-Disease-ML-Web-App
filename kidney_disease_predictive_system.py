ae06d607504f6a11b84e1bdd1b53225ee8d70376
balogun99 <balogunishaq19@gmail.com>
2026-06-06 10:07:09 +0100

    Initial commit

---
+++ kidney_disease_predictive_system.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pickle
+
+# load the saved model
+loaded_model = pickle.load(open('/Users/macbookpro/Desktop/ML & DL & GENAI Projects/Machine Learning/ Kidney Disease Predictive System/kidney_model_data.sav'))
+
+input_data = (71,0.3,40.9,0,1,46.8,1622,1) # No Dialysis Present)
+input_data = (28,1.55,14.5,1,0,73.4,1096,0) # Dialysis Present
+
+# change the input data into a numpy array
+input_data_as_numpy_array = np.asarray(input_data)
+
+# reshape the array for predicting 2 instances
+input_data_reshaped = input_data_as_numpy_array(1,-1)
+
+# make prediction
+prediction = loaded_model.predict(input_data_reshaped)
+print(prediction)
+
+if (prediction[0] == 0):
+    print("No Dialysis Present")
+else:
+    print("Dialysis Present")
+
+if (prediction[1] == 1):
+    print("Dialysis Present")
+else:
+    print("No Dialysis Present")
---
+++ kidney_disease_predictive_system_web_app.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pickle
+import streamlit as st
+
+# load the saved model
+loaded_model = pickle.load(open('/Users/macbookpro/Desktop/ML & DL & GENAI Projects/Machine Learning/Kidney Disease Predictive System/kidney_model_data.sav', 'rb'))
+
+# create a function for prediction
+def kidney_disease_prediction(input_data):
+    # change the input array into a numpy array
+    input_data_as_numpy_array = np.asarray(input_data)
+    # reshape the array for predicting 2 instances
+    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+    prediction = loaded_model.predict(input_data_reshaped)
+    print(prediction)
+
+    if (prediction[0] == 0):
+        return "No Dialysis Present"
+    else:
+        return "Dialysis Present"
+    
+# Streamlit Interface
+def main():
+    # web title
+    st.title("Kidney Disease Prediction Web App")
+    # input from the user
+    Age = st.text_input("Age of an Individual")
+    Creatinine_Level = st.text_input("Creatinine Level")
+    BUN = st.text_input("BUN Level")
+    Diabetes = st.text_input("Diabetes Status")
+    Hypertension = st.text_input("Hypertension Status")
+    GFR = st.text_input("GFR Level")
+    Urine_Output = st.text_input("Urine Output Status")
+    CKD_Status = st.text_input("CKD Status")
+
+    diagnosis = ''
+
+    if st.button("Kidney Disease Prediction"):
+        diagnosis = kidney_disease_prediction([Age,Creatinine_Level,BUN,
+                                               Diabetes,Hypertension,GFR,
+                                               Urine_Output,CKD_Status])
+        st.success(diagnosis)
+
+if __name__ == '__main__':
+    main()
