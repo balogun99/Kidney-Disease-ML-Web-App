f8a029f76b0337961750a339f412053cf77fdefe
balogun99 <balogunishaq19@gmail.com>
2026-06-06 10:31:35 +0100

    Fixes

--- kidney_disease_predictive_system.py
+++ kidney_disease_predictive_system.py
@@ -2,7 +2,7 @@ import numpy as np
 import pickle
 
 # load the saved model
-loaded_model = pickle.load(open('/Users/macbookpro/Desktop/ML & DL & GENAI Projects/Machine Learning/ Kidney Disease Predictive System/kidney_model_data.sav'))
+loaded_model = pickle.load(open('kidney_model_data.sav', 'rb'))
 
 input_data = (71,0.3,40.9,0,1,46.8,1622,1) # No Dialysis Present)
 input_data = (28,1.55,14.5,1,0,73.4,1096,0) # Dialysis Present
